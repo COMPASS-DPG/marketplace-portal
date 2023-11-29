ff95765f44251ec9963347ef71954d0640d4c9bb
kh4l1d64 <khalid.m@esmagico.in>
2023-11-29 09:04:18 +0530

    Removed redis-store module

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
 import { PrismaModule } from '../prisma/prisma.module';
-import { RedisStoreModule } from 'src/redis-store/redis-store.module';
+// import { RedisStoreModule } from 'src/redis-store/redis-store.module';
 
 @Module({
   controllers: [AdminController],
