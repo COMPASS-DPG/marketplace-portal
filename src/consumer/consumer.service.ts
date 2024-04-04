5b0d7585d828c91889599cd803dbdadc9b340ece
kh4l1d64 <khalid.m@esmagico.in>
2024-04-04 13:47:59 +0530

    minor change

--- src/consumer/consumer.service.ts
+++ src/consumer/consumer.service.ts
@@ -35,9 +35,9 @@ export class ConsumerService {
             // if the consumer is not available in the marketplace models, fetch it from user service
             const baseUrl = "https://compass-dev.tarento.com/api/user/v4/user/search";
             const headers = {
-                'Authorization': 'bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiIzRjB4UmVWNTJLcTc3R0xycnlKT2N2cXQwNVpUZTYySyJ9.ia3j2Pr-IFuioVXzerZjSNwC3HKvj-YcwjvkxOUNx0o',
+                'Authorization': 'bearer ' + process.env.USER_SERVICE_TOKEN,
                 'Content-Type': 'application/json',
-                'Cookie': 'connect.sid=s%3AXThrvZBYAza6cyO1AN6v2_6KOL5EM5cI.tQhpI4ryxMsIAvhAW6A%2Fkc9pAr5sxoC41PnTwUemWP0'
+                'Cookie': process.env.USER_SERVICE_COOKIE
               };
               
             const data = {
@@ -119,9 +119,9 @@ export class ConsumerService {
         if(!consumer) {
             const baseUrl = "https://compass-dev.tarento.com/api/user/v4/user/search";
             const headers = {
-                'Authorization': 'bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiIzRjB4UmVWNTJLcTc3R0xycnlKT2N2cXQwNVpUZTYySyJ9.ia3j2Pr-IFuioVXzerZjSNwC3HKvj-YcwjvkxOUNx0o',
+                'Authorization': 'bearer ' + process.env.USER_SERVICE_TOKEN,
                 'Content-Type': 'application/json',
-                'Cookie': 'connect.sid=s%3AXThrvZBYAza6cyO1AN6v2_6KOL5EM5cI.tQhpI4ryxMsIAvhAW6A%2Fkc9pAr5sxoC41PnTwUemWP0'
+                'Cookie': process.env.USER_SERVICE_COOKIE
               };
               
             const data = {
